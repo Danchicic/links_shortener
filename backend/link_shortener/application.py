082022d8042cd66f446c7efad5a37cb2a9e1c4ba
danek52 <guskir3565@gmail.com>
2025-11-30 18:32:37 +0300

    add all endpoints to backend

--- backend/link_shortener/application.py
+++ backend/link_shortener/application.py
@@ -1,7 +1,10 @@
+import logging
+
 from fastapi import FastAPI
 
 from link_shortener.routes import router as main_router
 
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 app = FastAPI()
 app.include_router(main_router)
 
--- backend/link_shortener/routes.py
+++ backend/link_shortener/routes.py
@@ -1,9 +1,14 @@
+import logging
 import os
+import uuid
 
-from fastapi import APIRouter
-
-from link_shortener.schemas import LinkSchema
+from fastapi import APIRouter, HTTPException
 from redis.asyncio import Redis
+
+from link_shortener.response_schemas import LinkSchemaResponse
+from link_shortener.schemas import LinkSchema
+
+logger = logging.getLogger(__name__)
 
 router = APIRouter(prefix='/links', tags=['Link Shortener'])
 redis_instance: Redis = Redis(
@@ -15,9 +20,23 @@ redis_instance: Redis = Redis(
 
 @router.get("/{id}")
 async def get_link_by_id(id: str) -> str:
-    return "mew"
+    if long_link := await redis_instance.get(id):
+        return long_link
+    raise HTTPException(status_code=404, detail="Link not found")
 
 
 @router.post("/")
 async def create_link(link: LinkSchema) -> LinkSchema:
-    pass
+    link_string = link.link
+    uuid_string = str(uuid.uuid4())
+    try:
+        await redis_instance.set(
+            uuid_string,
+            link_string,
+            ex=24 * 60 * 60,  # 24 hours
+        )
+        return LinkSchemaResponse(link=f"http://localhost/links/{uuid_string}")
+
+    except Exception:
+        logger.exception("Failed to create link")
+        raise HTTPException(status_code=400, detail="Failed to create link")
