5af6537425e3316a308a3e08376d9b2039e38215
danek52 <guskir3565@gmail.com>
2025-11-30 18:34:17 +0300

    add cors for frontend

--- backend/link_shortener/application.py
+++ backend/link_shortener/application.py
@@ -1,6 +1,7 @@
 import logging
 
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from link_shortener.routes import router as main_router
 
@@ -8,7 +9,14 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(level
 app = FastAPI()
 app.include_router(main_router)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
 
+
+)
 @app.get("/liveness")
 async def liveness():
     return {"status": "active!"}
